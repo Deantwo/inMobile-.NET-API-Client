36f261ddc28291c934fccc97c37e621b1fabe1c9
Deantwo <2676134+Deantwo@users.noreply.github.com>
2020-02-19 08:58:49 +0100

    SmsMessage changes
    Added empty default constructor.
    Added Summary comments to constructors.
    Removed inconsistent property access. Should be checked in class usage instead.
    Is backward compatible.

--- Sms.ApiClient/V2/SendMessages/SmsMessage.cs
+++ Sms.ApiClient/V2/SendMessages/SmsMessage.cs
@@ -4,6 +4,24 @@ namespace Sms.ApiClient.V2.SendMessages
 {
     public class SmsMessage : ISmsMessage
     {
+        /// <summary>
+        /// Default empty constructor.
+        /// </summary>
+        public SmsMessage()
+        {
+        }
+        /// <summary>
+        /// Constructor with all required parameters set, and optional parameters set with a default value.
+        /// </summary>
+        /// <param name="msisdn">The phone number to send to, including country code, e.g. 4512345678</param>
+        /// <param name="text">The text message in the SMS.</param>
+        /// <param name="senderName">The sendername, keep this between 3 and 11 chars.</param>
+        /// <param name="encoding">The encoding to use.</param>
+        /// <param name="messageId">The message id used to identify the message.<br/>If NULL or empty string, a message id will be generated on the API server side and returned in the response.<br/>Optional.</param>
+        /// <param name="sendTime">The time for the message to be sent. use NULL to send immediately.<br/>Optional.</param>
+        /// <param name="flash">If true, message is a flash message. Optional.</param>
+        /// <param name="expireTimeSpan">The amount of time the message is valid for. If the time passes before the message is sent, the message will not be sent.<br/>Optional.</param>
+        /// <param name="respectBlacklist">Whether or not blacklisting should be respected when sending messages.<br/>Optional.</param>
         public SmsMessage(string msisdn, string text, string senderName, SmsEncoding encoding, string messageId = null, DateTime? sendTime = null, bool flash = false, TimeSpan? expireTimeSpan = null, bool? respectBlacklist = null)
         {
             MessageId = messageId ?? "";
@@ -18,7 +36,7 @@ namespace Sms.ApiClient.V2.SendMessages
         }
 
         /// <summary>
-        /// The message id used to identify the message.
+        /// The message id used to identify the message.<br/>
         /// If NULL or empty string, a message id will be generated on the API server side and returned in the response.
         /// </summary>
         public string MessageId { get; set; }
@@ -26,27 +44,27 @@ namespace Sms.ApiClient.V2.SendMessages
         /// <summary>
         /// If true, message is a flash message
         /// </summary>
-        public bool Flash { get; private set; }
+        public bool Flash { get; set; }
 
         /// <summary>
         /// The phone number to send to, including country code, e.g. 4512345678
         /// </summary>
-        public string Msisdn { get; private set; }
+        public string Msisdn { get; set; }
 
         /// <summary>
         /// The text message in the SMS
         /// </summary>
-        public string Text { get; private set; }
+        public string Text { get; set; }
 
         /// <summary>
         /// The sendername, keep this between 3 and 11 chars.
         /// </summary>
-        public string SenderName { get; private set; }
+        public string SenderName { get; set; }
 
         /// <summary>
         /// The encoding to use.
         /// </summary>
-        public SmsEncoding Encoding { get; private set; }
+        public SmsEncoding Encoding { get; set; }
 
         /// <summary>
         /// If NULL, the message is not overcharged.
@@ -59,7 +77,7 @@ namespace Sms.ApiClient.V2.SendMessages
         public DateTime? SendTime { get; set; }
 
         /// <summary>
-        /// The amount of time the message is valid for. If the time passes before the message is sent, the message will not be sent.
+        /// The amount of time the message is valid for. If the time passes before the message is sent, the message will not be sent.<br/>
         /// If null, validity period for the message, is not set.
         /// </summary>
         public TimeSpan? ExpireIn { get; set; }
@@ -67,6 +85,6 @@ namespace Sms.ApiClient.V2.SendMessages
         /// <summary>
         /// Whether or not blacklisting should be respected when sending messages.
         /// </summary>
-        public bool? RespectBlacklist { get; }
+        public bool? RespectBlacklist { get; set; }
     }
 }
